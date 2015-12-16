125686edab4efdf3268a9f04b566b0c4050f31a5
Vinod Kumar Kashyap <vinod.kashyap12@gmail.com>
2015-12-16 09:02:43 +0530

    new files

---
+++ spring-mongo/src/main/java/com/vinod/initializer/AppInitializer.java
@@ -0,0 +1,33 @@
+package com.vinod.initializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    private static final String CONFIG_LOCATION = "com.vinod.config";
+    private static final String MAPPING_URL = "/";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping(MAPPING_URL);
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation(CONFIG_LOCATION);
+        return context;
+    }
+
+}
